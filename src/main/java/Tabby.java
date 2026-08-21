71de904b873ef00e7ecfa1373b7ac993540d467e
Justin Chua <justinchua1973@gmail.com>
2026-08-21 15:52:03 +0800

    Level 4. ToDos, Events, Deadlines

--- src/main/java/Tabby.java
+++ src/main/java/Tabby.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 
 public class Tabby {
 
-    // Represents an individual task with a description and completion state
+    // Base Task class
     public static class Task {
 
-        private String description;
-        private boolean isDone;
+        protected String description;
+        protected boolean isDone;
 
         public Task(String description) {
             this.description = description;
@@ -24,10 +24,60 @@ public class Tabby {
             this.isDone = false;
         }
 
+        public String getStatusIcon() {
+            return (isDone ? "X" : " ");
+        }
+
         @Override
         public String toString() {
-            String statusIcon = isDone ? "X" : " ";
-            return "[" + statusIcon + "] " + description;
+            return "[" + getStatusIcon() + "] " + description;
+        }
+    }
+
+    // Subclass for ToDos
+    public static class Todo extends Task {
+
+        public Todo(String description) {
+            super(description);
+        }
+
+        @Override
+        public String toString() {
+            return "[T]" + super.toString();
+        }
+    }
+
+    // Subclass for Deadlines
+    public static class Deadline extends Task {
+
+        protected String by;
+
+        public Deadline(String description, String by) {
+            super(description);
+            this.by = by;
+        }
+
+        @Override
+        public String toString() {
+            return "[D]" + super.toString() + " (by: " + by + ")";
+        }
+    }
+
+    // Subclass for Events
+    public static class Event extends Task {
+
+        protected String from;
+        protected String to;
+
+        public Event(String description, String from, String to) {
+            super(description);
+            this.from = from;
+            this.to = to;
+        }
+
+        @Override
+        public String toString() {
+            return "[E]" + super.toString() + " (from: " + from + " to: " + to + ")";
         }
     }
 
@@ -38,10 +88,18 @@ public class Tabby {
             sb.append("     ")
                     .append(i + 1)
                     .append(".")
-                    .append(items.get(i).toString())
+                    .append(items.get(i))
                     .append("\n");
         }
         return sb.toString();
+    }
+
+    private static void printTaskAdded(String divider, Task task, int taskCount) {
+        System.out.println(divider);
+        System.out.println("     Got it. I've added this task:");
+        System.out.println("       " + task);
+        System.out.println("     Now you have " + taskCount + " tasks in the list.");
+        System.out.println(divider);
     }
 
     public static void main(String[] args) {
@@ -82,11 +140,29 @@ public class Tabby {
                 System.out.println("     OK, I've marked this task as not done yet:");
                 System.out.println("       " + task);
                 System.out.println(divider);
-            } else {
-                tasks.add(new Task(input));
-                System.out.println(divider);
-                System.out.println("     added: " + input);
-                System.out.println(divider);
+            } else if (input.startsWith("todo ")) {
+                String description = input.substring(5);
+                Task task = new Todo(description);
+                tasks.add(task);
+
+                printTaskAdded(divider, task, tasks.size());
+            } else if (input.startsWith("deadline ")) {
+                String[] parts = input.substring(9).split(" /by ");
+                Task task = new Deadline(parts[0], parts[1]);
+                tasks.add(task);
+
+                printTaskAdded(divider, task, tasks.size());
+            } else if (input.startsWith("event ")) {
+                String[] parts = input.substring(6).split(" /from ");
+                String description = parts[0];
+                String[] timeParts = parts[1].split(" /to ");
+                String from = timeParts[0];
+                String to = timeParts[1];
+
+                Task task = new Event(description, from, to);
+                tasks.add(task);
+
+                printTaskAdded(divider, task, tasks.size());
             }
         }
 
