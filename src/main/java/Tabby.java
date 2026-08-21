0c6b9422e1cd050caeb3ab57a043e0b4ade5c70e
Justin Chua <justinchua1973@gmail.com>
2026-08-21 11:31:56 +0800

    Level 1. Echo

--- src/main/java/Tabby.java
+++ src/main/java/Tabby.java
@@ -1,10 +1,32 @@
 
+import java.util.Scanner;
+
 public class Tabby {
 
     public static void main(String[] args) {
-        String banner = "Hello! I'm Tabby.\n"
-                + "What can I do for you?\n"
-                + "Bye. Hope to see you again soon!";
-        System.out.println(banner);
+        Scanner scanner = new Scanner(System.in); // initialise a scanner object
+        String divider = "    ____________________________________________________________";
+
+        System.out.println("     What can I do for you?");
+        System.out.println(divider + "\n");
+
+        while (scanner.hasNextLine()) {
+            String input = scanner.nextLine(); // wait for user input
+
+            System.out.println(divider);
+
+            // Compare strings in Java using .equals() instead of == or strcmp
+            if (input.equals("bye")) {
+                System.out.println("     Bye. Hope to see you again soon!");
+                System.out.println(divider);
+                break; // Exit the loop
+            }
+
+            // Echo input back
+            System.out.println("     " + input);
+            System.out.println(divider + "\n");
+        }
+
+        scanner.close();
     }
 }
