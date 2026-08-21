5f1c778458405a831ee6ed6920afa28aaca29b30
Justin Chua <justinchua1973@gmail.com>
2026-08-21 14:50:26 +0800

    Level 2. Add, List

--- src/main/java/Tabby.java
+++ src/main/java/Tabby.java
@@ -1,30 +1,51 @@
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Tabby {
 
+    public static String formatList(List<String> items) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < items.size(); i++) {
+            sb.append("     ")
+                    .append(i + 1)
+                    .append(". ")
+                    .append(items.get(i))
+                    .append("\n");
+        }
+
+        return sb.toString();
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in); // initialise a scanner object
         String divider = "    ____________________________________________________________";
+        List<String> a = new ArrayList<>();
 
         System.out.println("     What can I do for you?");
         System.out.println(divider + "\n");
 
         while (scanner.hasNextLine()) {
+
             String input = scanner.nextLine(); // wait for user input
 
-            System.out.println(divider);
-
-            // Compare strings in Java using .equals() instead of == or strcmp
             if (input.equals("bye")) {
+                System.out.println(divider);
                 System.out.println("     Bye. Hope to see you again soon!");
                 System.out.println(divider);
-                break; // Exit the loop
+                break;
+            } else if (input.equals("list")) {
+                System.out.println(divider);
+                System.out.println(formatList(a)); // Use .print() to avoid an extra trailing newline
+                System.out.println(divider);
+            } else {
+                a.add(input);
+                System.out.println(divider);
+                System.out.println("     added: " + input);
+                System.out.println(divider);
             }
-
-            // Echo input back
-            System.out.println("     " + input);
-            System.out.println(divider + "\n");
         }
 
         scanner.close();
