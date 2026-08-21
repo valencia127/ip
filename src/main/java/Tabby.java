2359ef0b5cc5d79a73fdd58c3e3d84d24d4ec89d
Justin Chua <justinchua1973@gmail.com>
2026-08-21 11:03:26 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Tabby.java
@@ -0,0 +1,10 @@
+
+public class Tabby {
+
+    public static void main(String[] args) {
+        String banner = "Hello! I'm Tabby.\n"
+                + "What can I do for you?\n"
+                + "Bye. Hope to see you again soon!";
+        System.out.println(banner);
+    }
+}
