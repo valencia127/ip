3d5c3ca4e3dccd8e86a1b0e85f7fa78ba5404c03
Justin Chua <justinchua1973@gmail.com>
2026-09-01 15:50:38 +0800

    Add JavaDoc comments

--- src/main/java/ip/exception/TabbyException.java
+++ src/main/java/ip/exception/TabbyException.java
@@ -1,8 +1,10 @@
 
 package ip.exception;
 
+/** Represents a user-facing error raised by the application. */
 public class TabbyException extends Exception {
 
+    /** Creates an application exception with the supplied message. */
     public TabbyException(String message) {
         super(message);
     }
--- src/main/java/ip/model/Task.java
+++ src/main/java/ip/model/Task.java
@@ -1,33 +1,40 @@
 
 package ip.model;
 
+/** Represents a task with a description and completion status. */
 public class Task {
 
     protected String description;
     protected boolean isDone;
 
+    /** Creates an incomplete task with the given description. */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /** Marks this task as completed. */
     public void markAsDone() {
         this.isDone = true;
     }
 
+    /** Marks this task as incomplete. */
     public void markAsNotDone() {
         this.isDone = false;
     }
 
+    /** Returns the display icon for this task's completion status. */
     public String getStatusIcon() {
         return isDone ? "X" : " ";
     }
 
+    /** Returns the serialized representation used by persistent storage. */
     public String toFileFormat() {
         return (isDone ? "1" : "0") + " | " + description;
     }
 
     @Override
+    /** Returns the user-facing representation of this task. */
     public String toString() {
         return "[" + getStatusIcon() + "] " + description;
     }
